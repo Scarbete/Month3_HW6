ec4b4c9fd6bbecba5843d71ba4ae18fc785e94ed
Scarbete <megavertal@gmail.com>
2023-12-23 21:58:03 +0600

    iguiweuhdiewu

--- app.js
+++ app.js
@@ -1,75 +1,35 @@
-console.log(4798894)
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const card = document.getElementById('card')
 
-// const randomNumsButton = document.querySelector('.randomNumsButton')
-// const box = document.querySelector('.box')
-//
-// // const num = 8
-// // console.log(num.toString().length)
-//
-// const getRandomInt = (min, max) => {
-//     min = Math.ceil(min)
-//     max = Math.floor(max)
-//     return Math.floor(Math.random() * (max - min + 1)) + min
-// }
-//
-// const renderCircle = () => {
-//     box.innerHTML = ""
-//
-//     for (let i = 0; i < 6; i++) {
-//
-//         const randomNumber = getRandomInt(1, 99)
-//         const circle = document.createElement("div")
-//
-//         const numLength = randomNumber.toString()
-//
-//         circle.innerHTML = `<div>${numLength.length === 1 ? `0${randomNumber}` : randomNumber}</div>`
-//         circle.classList.add('circle')
-//
-//         box.append(circle)
-//     }
-// }
-//
-// randomNumsButton.onclick = () => renderCircle()
-//
-//
-//
-// // const randomNumber = getRandomInt(1, 99)
-//
-// // console.log(randomNumber)
-//
-// // for (let i = 0; i < 6; i++) {}
-//
-// // function renderCircle(number) {
-// //     if number.length === 1
-// //     return `
-// //         <div>${number}</div>
-// //     `
-// // }
-//
-// // const buttons = document.querySelectorAll('button')
-// //
-// // buttons.forEach(item => {
-// //     item.onclick = () => {
-// //         document.body.style.backgroundColor = item.innerText
-// //     }
-// // })
-// //
-// // const nums = [ 13,13,1,23,1,3,24,86,75,6,24,13 ]
-// // const newArr = []
-// //
-// // for (const numsKey of nums) {
-// //     !newArr.includes(numsKey) && newArr.push(numsKey)
-// // }
-// //
-// //
-// //
-// // console.log(newArr)
-// //
-// // console.log(nums.sort((a, b) => a - b))
-// //
-// // console.log(nums.sort((a, b) => b - a))
-// //
-// // console.log(nums.toSorted((a, b) => a - b))
-// //
-// // console.log(nums.reverse(), 'reverse()')
-//
+const BASE_URL = `https://jsonplaceholder.typicode.com/todos`
+let limit = 1
+let page = 1
+
+const renderData = (data) => {
+    data.forEach(item => {
+        card.innerHTML = `
+            <h3>${item.id}</h3>
+            <p>${item.title}</p>
+            <p></p>
+        `
+    })
+}
+
+const getData = () => {
+    fetch(`${BASE_URL}?_limit=${limit}&_page=${page}`)
+        .then(response => response.json())
+        .then(data => renderData(data))
+}
+
+getData()
+
+prev.onclick = () => {
+    page--
+    page > 0 && getData()
+}
+
+next.onclick = () => {
+    page++
+    page < 200 && getData()
+}
